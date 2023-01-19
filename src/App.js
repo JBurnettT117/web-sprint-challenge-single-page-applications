390181180a40d7bb18f91d2a01eaf80e67f009cc
Omen <joshuaburnett429@gmail.com>
2023-01-19 12:08:08 -0600

    transferring current files to desktop

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
 
+import "./App.css"; 
+
 const App = () => {
   return (
     <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+      <div className="Header">
+        <div className="Title">
+          <h1>Lambda Eats</h1>
+        </div>
+        <div className="Navigation">
+          <h4 className="Nav-link"/**put on click */>Home</h4>
+          <h4 className="Nav-link"/**put on click */>Help</h4>
+        </div>  
+      </div>
     </>
   );
 };
